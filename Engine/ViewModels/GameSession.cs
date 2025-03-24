21a3169840a8fd88a24c9c1df345e2597d5280fb
jkirigo <jkirigo@seaburymro.com>
2025-03-24 09:50:32 +0300

    Role Playing game in WPF C#

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModel
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Jeffrey";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+
+        }
+    }
+}
